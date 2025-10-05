3b12548e6c2676d45fad1233a557a6ca6cf0ec36
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-10-05 12:56:29 +0530

    Create Online GPU account

---
+++ train.py
@@ -0,0 +1,36 @@
+import sys
+
+import modal
+
+app = modal.App("audio-cnn")
+
+image = (model.Image.debian_slim()
+         .pip_install_from_requirements("requirements.txt")
+         .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
+         .run_commands([
+             "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip",
+             "cd /tmp && unzip esc50.zip",
+             "mkdir -p /opt/esc50-data",
+             "cp -r /tmp/ESC-50-master/* /opt/esc50-data/",
+             "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
+         ])
+         )
+
+@app.function()
+def f(i):
+    if i % 2 == 0:
+        print("hello", i)
+    else:
+        print("world", i, file=sys.stderr)
+
+    return i * i
+
+@app.local_entrypoint()
+def main():
+    # run the function locally
+    print(f.local(10))
+
+    # run the function remotely on Modal
+    print(f.remote(10))
+
+
