dbcc7359020266cc30faf5c3674b1b89590dbba8
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-10-05 20:31:08 +0530

    Model traning done

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -6,19 +7,16 @@ class ResidualBlock(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels,
                                3, stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNormal2nd(out_channels)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels,
                                3, padding=1, bias=False)
-        self.bn2 = nn.BatchNormal2nd(out_channels)
-        self.shortcut = nn.Sequential()
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
+        self.shortcut = nn.Sequential()
         self.use_shortcut = stride != 1 or in_channels != out_channels
         if self.use_shortcut:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1,
-                          stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False), nn.BatchNorm2d(out_channels))
 
     def forward(self, x, fmap_dict=None, prefix=""):
         out = self.conv1(x)
@@ -30,49 +28,31 @@ class ResidualBlock(nn.Module):
         out_add = out + shortcut
 
         if fmap_dict is not None:
-            fmap_dict[f"{prefix}.con"] = out_add
+            fmap_dict[f"{prefix}.conv"] = out_add
 
         out = torch.relu(out_add)
         if fmap_dict is not None:
-            fmap_dictp[f"{prefix}".relu] = out
+            fmap_dict[f"{prefix}.relu"] = out
 
         return out
 
 
 class AudioCNN(nn.Module):
-    def __init__(self, num_class=50):
+    def __init__(self, num_classes=50):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False),
-            nn.BatchNom2d(63),
-            nn.ReLu(inplace=True),
-            nn.MaxPool2d(3, stride=2, padding=1)
-        )
+            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True), nn.MaxPool2d(3, stride=2, padding=1))
         self.layer1 = nn.ModuleList([ResidualBlock(64, 64) for i in range(3)])
         self.layer2 = nn.ModuleList(
-            [
-                ResidualBlock(64 if i == 0 else 128, 128,
-                              stride=2 if i == 0 else 1)
-                for i in range(4)
-            ])
+            [ResidualBlock(64 if i == 0 else 128, 128, stride=2 if i == 0 else 1) for i in range(4)])
         self.layer3 = nn.ModuleList(
-            [
-                ResidualBlock(128 if i == 0 else 256, 256,
-                              stride=2 if i == 0 else 1)
-                for i in range(6)
-            ]
-        )
+            [ResidualBlock(128 if i == 0 else 256, 256, stride=2 if i == 0 else 1) for i in range(6)])
         self.layer4 = nn.ModuleList(
-            [
-                ResidualBlock(256 if i == 0 else 512, 512,
-                              stride=2 if i == 0 else 1)
-                for i in range(3)
-            ]
-        )
+            [ResidualBlock(256 if i == 0 else 512, 512, stride=2 if i == 0 else 1) for i in range(3)])
 
-        self.avgpool = nn.AdaptivePool2d((1, 1))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(512, num_class)
+        self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x, return_feature_maps=False):
         if not return_feature_maps:
@@ -87,8 +67,32 @@ class AudioCNN(nn.Module):
                 x = block(x)
             x = self.avgpool(x)
             x = x.view(x.size(0), -1)
-            x = self.dropout(x) 
+            x = self.dropout(x)
             x = self.fc(x)
+            return x
+        else:
+            feature_maps = {}
+            x = self.conv1(x)
+            feature_maps["conv1"] = x
 
-            return x , feature_maps
-            
+            for i, block in enumerate(self.layer1):
+                x = block(x, feature_maps, prefix=f"layer1.block{i}")
+            feature_maps["layer1"] = x
+
+            for i, block in enumerate(self.layer2):
+                x = block(x, feature_maps, prefix=f"layer2.block{i}")
+            feature_maps["layer2"] = x
+
+            for i, block in enumerate(self.layer3):
+                x = block(x, feature_maps, prefix=f"layer3.block{i}")
+            feature_maps["layer3"] = x
+
+            for i, block in enumerate(self.layer4):
+                x = block(x, feature_maps, prefix=f"layer4.block{i}")
+            feature_maps["layer4"] = x
+
+            x = self.avgpool(x)
+            x = x.view(x.size(0), -1)
+            x = self.dropout(x)
+            x = self.fc(x)
+            return x, feature_maps
